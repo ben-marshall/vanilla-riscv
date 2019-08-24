05ff97ef127d6bc549e6c5a56f1e777f459e1950
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-08-24 13:53:59 +0100

    Add string printing/perf reporting to embench.
     On branch master
     Your branch is ahead of 'origin/master' by 2 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   flow/embench/Makefile.in
    	new file:   flow/embench/embench.c
    	modified:   flow/embench/embench.h
    	modified:   flow/embench/main.c

--- flow/embench/main.c
+++ flow/embench/main.c
@@ -8,20 +8,31 @@ int main() {
     volatile int result;
     int correct;
 
+    __putstr("Initialise Bechmark...\n");
+
     initialise_benchmark ();
+    
+    __putstr("Run Bechmark...\n");
 
     uint64_t i_start = __rdinstret();
-    uint64_t c_start = __rdinstret();
+    uint64_t c_start = __rdcycle();
     
     result = benchmark ();
     
     uint64_t i_end   = __rdinstret();
-    uint64_t c_end   = __rdinstret();
+    uint64_t c_end   = __rdcycle();
 
     uint64_t count_instrs = i_end - i_start;
     uint64_t count_cycles = c_end - c_start;
 
+    __putstr("Cycles: 0x"); __puthex64(count_cycles); __putchar('\n');
+    __putstr("Instrs: 0x"); __puthex64(count_instrs); __putchar('\n');
+    
+    __putstr("Verify Bechmark...\n");
+
     correct = verify_benchmark (result);
+    
+    __putstr("--- Finished --- \n");
 
     return (!correct);
 
